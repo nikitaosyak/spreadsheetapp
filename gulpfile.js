e9d82666786d38a14f46d31b332c05fb0b404f62
grozamorei <desperatebassplayer@gmail.com>
2017-06-23 09:39:01 +0300

    added code validation with jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 const gulp = require('gulp')
 const shell = require('gulp-shell')
+const jshint = require('gulp-jshint')
 
-gulp.task('deploy', shell.task('gapps upload'))
+gulp.task('lint', () => {
+    return gulp.src('./src/*.js*')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish', {beep: true}))
+        .pipe(jshint.reporter('fail'))
+})
+
+gulp.task('deploy', ['lint'], shell.task('gapps upload'))
 
 gulp.task('watch', () => {
     gulp.watch(['src/**/*'], ['deploy'])
